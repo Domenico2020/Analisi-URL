6d7418a6e5551d232beeee979e3e67a06318fb2a
Domenico2020 <74300495+Domenico2020@users.noreply.github.com>
2021-01-18 18:16:19 +0100

    Aggiornamento con modifica Output
    Creazione Classi e metodi, lettura in input di un file Txt ad esempio abbiamo caricato il file lista2.txt.
    Filtraggio input console per la scelta del sito da anaizzare.
    Output del file è un file json contenente la lista degli  HyperLinks con profondità 1, ovvero quella base, il testo del file contiene anche il sito selezionato per l'analisi.
    
    Co-Authored-By: Egidio92 <74206267+Egidio92@users.noreply.github.com>

--- Main1.py
+++ Main1.py
@@ -10,43 +10,79 @@ Si scrive un algoritmo che dato un url in ingresso restituisce la lista degli
 hyperlink presenti nell'url'.
 """
 
-import requests
 import argparse
+from abc import ABC, abstractmethod
+from os.path import splitext
+import os
 import json
 import re
-#import pandas as pd
-    
+import requests
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-o1", "--out_data1", help="Complete path il file input_data.json", 
-                     type=str, default='./FileProva1.json')
-parser.add_argument("-o2", "--out_data2", help="Complete path il file input_data.json", 
+parser.add_argument("-i", "--in_data", help="Complete path il file input_data.json", 
+                     type=str, default='./lista2.txt')
+parser.add_argument("-o", "--out_data", help="Complete path il file input_data.json", 
                      type=str, default='./Lista_HyperLinks.json')
 args = parser.parse_args()
 
+class UrlReader(ABC):
+    """
+    interface
+    """
+
+    @abstractmethod
+    def get_list_of_url(self):
+        """
+        abstract method
+        """
+        pass
+
+    @staticmethod
+    def create_instance(filename):
+        suffix = splitext(filename)[1][1:].lower()
+        if suffix == 'txt':
+            return TXTReader(filename)
+        else:
+            raise ValueError('unknown file type')
+        
+
+class TXTReader(UrlReader):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_list_of_url(self):
+        fin = open(self.filename, 'r')
+        text = fin.read()
+        return text
+            
+# acquisisci lista di url e memorizzala in una lista
+list_url = []
+reader = UrlReader.create_instance(args.in_data)
+stringa = reader.get_list_of_url()
+list_url = re.findall(r'(https?://[^\s]+)', stringa)
+
+print('Questi sono gli Url:')
+print(list_url)
+print('Inserisci il nome del sito da Analizzare:')
+a = str(input())
+print(' ')
+print('Il sito inserito è: ' +a)
+print(' ')
 # Si analizza l'url
-Url= requests.get('http://www.sialserramentisrl.it')
-
-
-# Lanciata una richiesta, la libreria Requests fa delle ipotesi sull'encoding 
-# della risposta sulla base degli header HTTP. L’encoding del testo utilizzato
-# da Requests si applica al contenuto di "Url.text". Si può sapere quale 
-# encoding viene usato dalla libreria Request (ed anche cambiarlo) utilizzando
-# il codice "Url.encoding" 
-
+url_rqst = requests.get(a)
 # Si verifica la disponibilità della pagina
-Status_Url=Url.status_code
-if  Status_Url == 200:
-    Html=Url.text
-    Info_Url=Url.headers
-    Links = re.findall('''<a\s+(?:[^>]*?\s+)?href="([^"]*)"''', str(Html))
-    #Info=pd.DataFrame(Info_Url)
-    print(Info_Url)
+status_url = url_rqst.status_code
+if  status_url == 200:
+    html = url_rqst.text
+    new_links = re.findall('''<a\s+(?:[^>]*?\s+)?href="([^"]*)"''', str(html))
 else:
-    print('Error 404 NOT FOUND')
- 
-# Si esporta il file in formato json
-with open(args.out_data1, 'w') as f:
-    json.dump(Html, f)
+    # stampa le pagine non disponibili
+    print('--------- off-line'+str(a)) 
 
-with open(args.out_data2, 'w') as f:
-    json.dump(Links, f, indent=3)
+a = a.replace("http://", "_")
+a = a.replace("https://", "_")
+estx = os.path.splitext(args.out_data)[1]   
+args.out_data = os.path.splitext(args.out_data)[0]+a+'_'+estx
+with open(args.out_data, 'w') as f:
+    json.dump(new_links, f, indent=3)
