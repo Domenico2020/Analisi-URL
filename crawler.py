553cc32adbb0f7a6f763543d4b60e73bd578bdec
Egidio92 <74206267+Egidio92@users.noreply.github.com>
2021-02-06 17:19:30 +0100

    Aggiornamento file relativo alle Issue aperte

---
+++ crawler.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 00:59:44 2021
+
+@author: Egidio
+"""
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+"""
+La classe DFSCrawler riceve in input un url che tramite una visita in profondità 
+analizza l'url e tutte le pagine che questo riferisce definendo la lista degli
+hyperlink (list_of_links) e la lista dei cicli (list_of_cycles) collegati all'url  
+ricevuto come input
+"""
+class DFSCrawler():
+     
+    def __init__(self):
+        self.list_of_links = []
+        self.list_of_cycles = []
+    
+    # il crawler
+    def get_links_recursively(self, base, path, visited, max_depth):
+        if 0 < max_depth:
+            try:
+                soup = BeautifulSoup(requests.get(base + path).text, "html.parser")
+                for link in soup.find_all("a"):
+                    href = link.get("href")
+                    if href not in visited:
+                        visited.add(href)                      
+                        if href is not None:
+                            # Non si considerano i link alla mail
+                            if href.startswith('mailto:'):
+                                pass
+                            # Non si considerano i link al telefono
+                            elif href.startswith('tel'):
+                                pass
+                            else: 
+                                if not href.startswith('http'):
+                                    joint = urljoin(base, href)
+                                    self.list_of_links.append(joint)                               
+                                if not href.startswith('https'):
+                                    joint = urljoin(base, href)
+                                    self.list_of_links.append(joint)
+                                if href.startswith('http'):
+                                    self.list_of_links.append(href)
+                                if href.startswith('https'):
+                                    self.list_of_links.append(href)
+                        print(f"at depth {max_depth}: {href}")
+                        if href.startswith("http"):                   
+                            self.get_links_recursively(href, "", visited, max_depth-1)
+                        else:
+                            self.get_links_recursively(base, href, visited, max_depth-1)
+                    else:
+                        visited.add(href)                      
+                        if href is not None:
+                            # Non si considerano i link alla mail
+                            if href.startswith('mailto:'):
+                                pass
+                            # Non si considerano i link al telefono
+                            elif href.startswith('tel'):
+                                pass
+                            else: 
+                                if not href.startswith('http'):
+                                    joint = urljoin(base, href)
+                                    self.list_of_cycles.append(joint)                               
+                                if not href.startswith('https'):
+                                    joint = urljoin(base, href)
+                                    self.list_of_cycles.append(joint)
+                                if href.startswith('http'):
+                                    self.list_of_cycles.append(href)
+                                if href.startswith('https'):
+                                    self.list_of_cycles.append(href)
+            except:
+                pass
+                
+
