dc4f85381c1d8c1091a3f287c34f7f3ef5e0b25c
Domenico2020 <74300495+Domenico2020@users.noreply.github.com>
2021-02-06 12:09:08 +0100

    Switch Branch

---
+++ ListaUrl23.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 22:19:22 2021
+
+@authors: Egidio e Domenico
+"""
+
+
+import requests
+import argparse
+import json
+from reader import UrlReader
+from crawler import DFSCrawler
+from cleaner import ListCleaner
+
+"""
+Si utilizza il parser per la lettura dei file in input e per la scrittura dei file in output
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--in_data", help="Scrivere il path completo per il file di input", 
+                    default='./data/Cartella.xlsx')
+parser.add_argument("-o", "--out_data", help="Scrivere il path completo per il file di output", 
+                    default='./Lista_HyperLinks.json')
+parser.add_argument("-url", "--colonna_Url", help="Scrivere la colonna contenente gli Url", 
+                    type=str, default='URL')
+parser.add_argument("-md", "--max_depth", help="Scrivere la profondità massima a cui arrivare", 
+                    type=int, default=2)
+args = parser.parse_args()
+
+
+
+"""
+MAIN
+"""
+
+# Si acquisisce la lista degli url 
+reader = UrlReader.create_instance(args.in_data , args.colonna_Url)
+list_of_urls = reader.get_list_of_url()
+
+# Si estraggono gli hyperlink presenti in ogni url e li si salva in una lista
+extractor = DFSCrawler()
+for url in list_of_urls:
+    print(' ')
+    print(' ')
+    print(url)  
+    extractor.list_of_links.append(
+                    "-------------- I LINK SEGUENTI FANNO RIFERIMENTO A: "
+                    + url + " --------------")
+    extractor.get_links_recursively(url, "", set([url]), args.max_depth)
+list_of_links = extractor.list_of_links
+list_of_cycles = extractor.list_of_cycles
+list_of_error = extractor.list_of_error
+# Si eliminano dalla lista dei link i valori non voluti
+cleaner = ListCleaner()
+list_of_links = cleaner.drop_duplicates(list_of_links)
+list_of_links = cleaner.drop_none_values(list_of_links)
+
+for errore in list_of_cycles:
+     if errore.startswith('http'):
+         r= requests.get(errore)
+     else:
+         r= requests.get('https://www.'+errore)
+     if not r == 200:
+         list_of_error = list_of_error.append(errore)
+         
+data = {
+    "Le liste in uscita sono": {
+        "Lista di link": list_of_links ,
+        "Lista di cicli": list_of_cycles
+    }
+}
+
+# Si salva la lista di hyperlink in un file json
+with open(args.out_data, 'w') as f:
+    json.dump(data, f, indent=3)
---
+++ reader.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 13:20:09 2021
+
+@author: Egidio e Domenico
+"""
+
+from abc import ABC, abstractmethod
+import re
+from os.path import splitext
+import pandas as pd 
+"""
+Si crea la classe astratta che rimanda tramite il metodo statico alle classi TXTReader
+ed XLSXReader. Questa classe contiene il metodo astratto get_list_of_url() che segue 
+una differente implementazione in base al suffiso del file in input.
+"""
+
+class UrlReader(ABC):
+    """
+    interface
+    """
+
+    @abstractmethod
+    def get_list_of_url(self):
+        """
+        abstract method
+        """
+        pass
+
+    @staticmethod
+    def create_instance(filename,colonna):
+        suffix = splitext(filename)[1][1:].lower()
+        if suffix == 'txt':
+            return TXTReader(filename)
+        elif suffix == 'xlsx':
+            return XLSXReader(filename,colonna)
+        else:
+            raise ValueError('unknown file type')
+
+"""
+La sottoclasse TXTReader legge un file .txt, contenente una lista di url, e 
+restituisce una lista python contenente quella lista di url
+"""
+
+class TXTReader(UrlReader):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_list_of_url(self):
+        fin = open(self.filename, 'r') 
+        list_of_urls_temp = fin.readlines()
+        list_of_urls = []
+        for url in list_of_urls_temp:
+            url = re.findall(r'(https?://[^\s]+)', url)[0]
+            list_of_urls.append(url)
+        return list_of_urls
+    
+"""
+La sottoclasse XLSXReader legge un file .xlsx, contenente una lista di url, e 
+restituisce una lista python contenente quella lista di url
+"""
+    
+class XLSXReader(UrlReader):
+
+    def __init__(self, filename , colonna):
+        self.filename = filename
+        self.colonna  = colonna
+    
+
+    def get_list_of_url(self):
+        fin = pd.ExcelFile(self.filename) 
+        sheetX = fin.parse(0)
+        list_of_urls = sheetX[self.colonna].tolist()
+        return list_of_urls
